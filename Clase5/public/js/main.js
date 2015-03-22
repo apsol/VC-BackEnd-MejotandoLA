30a6584845f0e5bda79bd4332259aa5e426898eb
apsol <apscarlos@hotmail.com>
2015-03-22 12:46:29 -0300

    Clase 5, notificación al cargar el modelo.
    Nos informa en consola cada vez que se agrega un cado a la colección.

--- Clase5/public/js/main.js
+++ Clase5/public/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 	console.log('main.js loaded');
+
+	window.collections.articles = new Puls3.Collections.Articles();
+	
+	window.collections.articles.on('add', function (model){
+		console.log('Se ha agregado', model.toJSON() );
+	} );
+
+
+	window.collections.articles.fetch();
 });
