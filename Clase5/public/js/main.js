b998f0de4b003458cb829065de98d24d48c4f1bc
apsol <apscarlos@hotmail.com>
2015-03-23 00:29:33 -0300

    Clase 5, se graba y actualiza a todos los navegador
    En este caso, se llena el formulario, se envia al server y luego se
    carga la web con socket y se renderea  la WEB

--- Clase5/public/js/main.js
+++ Clase5/public/js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 	console.log('main.js loaded');
 
+	window.ponyExpress = new PonyExpress({
+		io : window.location.origin
+	});
+
+	window.ponyExpress.bind('connect', function(){
+		window.plugs.article = new PonyExpress.BackbonePlug({
+			collection : window.collections.articles
+		});
+	});
+
 	window.views.app = new Puls3.Views.App( $('body') );
 
 	window.collections.articles = new Puls3.Collections.Articles();
