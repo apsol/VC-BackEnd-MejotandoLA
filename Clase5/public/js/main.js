27d8d86698004501e9a52b7fd2265d1cbe5e6b2c
apsol <apscarlos@hotmail.com>
2015-03-22 23:24:32 -0300

    Clase 5, guardando artículos en el server
    solo los guarda en el server,  no los carga en la pagina, solo se ven
    cuando se actualizan...

--- Clase5/public/js/main.js
+++ Clase5/public/js/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 	console.log('main.js loaded');
 
+	window.views.app = new Puls3.Views.App( $('body') );
+
 	window.collections.articles = new Puls3.Collections.Articles();
 	
 	window.collections.articles.on('add', function (model){
 		//agregamos nuevas vistas aqui...
 		var view = new Puls3.Views.Article( {model: model})
 		view.render();
-		view.$el.appendTo('.posts');
+		view.$el.prependTo('.posts');
 
 	} );
 
