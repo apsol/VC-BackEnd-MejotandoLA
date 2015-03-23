61b473ed1e746ddf869276e0372c016713afe2c0
apsol <apscarlos@hotmail.com>
2015-03-22 21:50:50 -0300

    Clase 5, creando vistas
    Cada ves que  add a un modelo, se  renderea en la pagina
    automaticamente, versión inicial.
    m = window.collections.articles.add({user: "Pedro", title : "Una mas
    HTML"})

--- Clase5/public/js/main.js
+++ Clase5/public/js/main.js
@@ -4,7 +4,11 @@ $(document).ready(function(){
 	window.collections.articles = new Puls3.Collections.Articles();
 	
 	window.collections.articles.on('add', function (model){
-		console.log('Se ha agregado', model.toJSON() );
+		//agregamos nuevas vistas aqui...
+		var view = new Puls3.Views.Article( {model: model})
+		view.render();
+		view.$el.appendTo('body');
+
 	} );
 
 
